89dc837ed4a39b0339c8a6ad42c972d0220a7c2b
Michael Luciuk <mrl280@mail.usask.ca>
2022-12-10 18:41:14 -0500

    Grover for minimization is working, but not enough qubits for large numbers

--- grover_for_minimization.py
+++ grover_for_minimization.py
@@ -1,6 +1,7 @@
 """
 Gover search algorithm for minimization
 """
+import math
 
 import pennylane as qml
 from numpy import pi as pi
@@ -9,69 +10,61 @@ from numpy import pi as pi
 def minimization_oracle(data_register: list[int], ancillary_register: list[int], arr: list[int], x: int) -> None:
     """
     Marking oracle to flip the signs of the elements that satisfy the minimization condition:
-        .
+        Mark states |i> (take |i> to -|i>) if arr[i] < x.
 
     :param data_register: List of ints:
         Our main data qubits.
     :param ancillary_register: List of ints:
         Our helper qubits.
-    :param arr: List of ints:
-        # TODO
-    :param x: int:
-        # TODO
+    :param arr: List of ints.
+    :param x: int.
 
     :return: None, operation is done in place.
     """
 
-    def add_k_fourier(k: int, wires: list[int]):
+    def add_k_fourier(k: int, wires: list[int]) -> None:
         """
-        Add k from wires.
-
-        :param k: int:
-            The value to add.
-
-        :param wires:
-            The register to add k to.
+        Add the integer value k to wires.
+        :return: None, operation is done in place.
         """
         for j in range(len(wires)):
             qml.RZ(k * pi / (2 ** j), wires=wires[j])
 
     def load_values_in_arr():
         """
-        ghf.
+        Conditionally load all the values in arr onto the axcillary register.
         """
-        # Perform a QFT, so we can add property values in the Fourier basis.
+        # Perform a QFT, so we can add values in the Fourier basis.
         qml.QFT(wires=ancillary_register)
 
-        # Loop through each of the data registers, preforming controlled additions
-        for wire in data_register:
+        # Loop through each of the data wires, preforming controlled additions.
+        for wire in range(len(arr)):
             qml.ctrl(op=add_k_fourier, control=wire)(k=arr[wire], wires=ancillary_register)
 
         # Return to the computational basis
         qml.adjoint(fn=qml.QFT(wires=ancillary_register))
 
     load_values_in_arr()
-    # If the current value of the ancillary register is a "correct solution", flip the sign
 
-    # If the ancillary_register contains a value smaller than x, we need to flip the sign.
-    for i in range(0, x):
-        if i in arr:
-            qml.FlipSign(i, wires=ancillary_register)
+    # Loop thorugh all integers in the range 1 to x-1. Notice we start at 1 because the all 0's state would always be
+    #  maked. Anyway, arr should never contain any 0's. If we get a sum of elements on the ancillary wires that is < x,
+    #  then there must be at least one element in arr that is less than x.
+    for i in range(1, x):
+        qml.FlipSign(i, wires=ancillary_register)
+
     qml.adjoint(fn=load_values_in_arr)()  # Cleanup.
 
 
 def minimization_circuit(data_register: list[int], ancillary_register: list[int], arr: list[int], x: int) -> list:
     """
-    A quantum circuit to mind a value smaller than x.
+    A quantum circuit to find a value in arr that is less than x.
 
     :param data_register: List of ints:
         Our main data qubits.
     :param ancillary_register: List of ints:
         Our helper qubits.
-    :param arr: list of ints:
-        # TODO
-    :param x: int:
-        # TODO
+    :param arr: list of ints.
+    :param x: int.
 
     :return: list:
         A flat array containing the probabilities of measuring each basis state.
@@ -82,7 +75,7 @@ def minimization_circuit(data_register: list[int], ancillary_register: list[int]
         qml.Hadamard(wires=wire)
 
     # for _ in range(4):
-    # Step 2: Use the oracle to mark elements that < x.
+    # Step 2: Use the oracle to mark solution states.
     minimization_oracle(data_register=data_register, ancillary_register=ancillary_register, arr=arr, x=x)
 
     # Step 3: Apply the Grover operator to amplify the probability of getting the correct solution.
@@ -91,22 +84,34 @@ def minimization_circuit(data_register: list[int], ancillary_register: list[int]
     return qml.probs(wires=data_register)
 
 
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
+def grover_for_minimization(arr: list[int], x: int) -> bool:
+    """
+    Use Grover's search to check if arr contains an element < x.
 
-    arr_ = [12, 10, 6, 7]
-    x_ = 10
+    Important Precondition:
+        arr must contain integer values strickly > 0.
 
-    # With 4 qubits we can represent 16 numbers (0 -> 15)
-    data_register_ = [0, 1, 2, 3]  # 6 qubits
-    ancillary_register_ = [4, 5, 6, 7]  # 4 work qubits
+    :return: bool:
+        True: We found an element in arr < x.
+        False: otherwise.
+    """
+    # We need enough qubits to represent 0 -> sum(arr). You can represent up to 2^n - 1 with n bits
+    # This makes sure we can represent all sum compbinations with no wrapping
+    number_qubits_required = math.ceil(math.log(sum(arr) + 1, 2))
+    # print("number of qubits required:")
+    # print(number_qubits_required)
 
-    device = qml.device("default.qubit", wires=data_register_ + ancillary_register_)
+    data_register = list(range(0, number_qubits_required))
+    ancillary_register = list(range(number_qubits_required, 2 * number_qubits_required))  # Oracle work space.
+    # print(data_register)
+    # print(ancillary_register)
 
+    device = qml.device("default.qubit", wires=data_register + ancillary_register)
     qnode = qml.qnode(func=minimization_circuit, device=device)(
-        data_register=data_register_, ancillary_register=ancillary_register_, arr=arr_, x=x_)
+        data_register=data_register, ancillary_register=ancillary_register, arr=arr, x=x)
 
-    values = qnode.__call__(data_register=data_register_, ancillary_register=ancillary_register_, arr=arr_, x=x_)
+    # Go ahead and actually run it.
+    values = qnode.__call__(data_register=data_register, ancillary_register=ancillary_register, arr=arr, x=x)
 
     print("\nlen(values):")  # Four bits can encode 16 distinct characters
     print(len(values))
@@ -116,3 +121,31 @@ if __name__ == "__main__":
     plt.bar(range(len(values)), values)
 
     plt.show()
+
+    # if result < x:
+    #     # If the returned value is actually less than result, great!
+    #     return True
+    # else:
+    #     # No such element exists.
+    #     return False
+
+    return False
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    arr_ = [18, 10, 6, 7]
+    # arr_ = [3, 5, 4, 3]
+    x_ = 63
+
+    found_number_smaller_than_x = grover_for_minimization(arr=arr_, x=x_)
+
+    print("Found a number smaller than x:")
+    print(found_number_smaller_than_x)
+
+    # Number of qubits should depend on the sum of arr - make sure we don't roll over!
+
+    # With 4 qubits we can represent 16 numbers (0 -> 15)
+    data_register_ = [0, 1, 2, 3]  # 6 qubits
+    ancillary_register_ = [4, 5, 6, 7]  # 4 work qubits
