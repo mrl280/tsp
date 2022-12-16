1efd7c1d2eaa84711c95b5d69db021a26c3111b9
Michael Luciuk <mrl280@mail.usask.ca>
2022-12-15 22:29:16 -0500

    Phase estimation working for the 2x2 case.

--- phase_estimation.py
+++ phase_estimation.py
@@ -1,4 +1,7 @@
 """
+Author: Michael Luciuk
+Date    Dec, 2022
+
 Here, we investigate using phase estimation to obtain the cycle costs.
 
 Notice this is something of an academic exercise to gain an improved understanding of phase estimation, as our problem
@@ -7,65 +10,130 @@ Notice this is something of an academic exercise to gain an improved understandi
 
 However, there are other approaches, such as the one presented in Srinivasan et. al.
  (https://arxiv.org/abs/1805.10928), where they only use the Hamiltonian cycles to gain insight into which of their n^n
- eigenvalues correspond to the (n - 1)! Hamiltonian cycles. As a consequence, they have to perform phase estimation with
+ eigenvalues correspond to the (n - 1)! Hamiltonian cycles. As a consequence, they perform phase estimation, and with
  a much larger U matrix then we will here.
 """
+import math
+
+import pennylane as qml
+from pennylane.templates import QuantumPhaseEstimation
+from pennylane import numpy as np
+
 from typing import Tuple
 
-import numpy as np
-from numpy.typing import ArrayLike
 
-
-def build_U(cycle_costs: ArrayLike) -> ArrayLike:
+def build_U(A: list[list[int]], verbose: False) -> Tuple[np.asarray, Tuple[float, float]]:
     """
     Build a unitary matrix U, wherein the costs are encoded as phases.
 
-    :param cycle_costs:
-        A 1-d array of len((n - 1)!) (where n is the number of cities) containing cycle costs.
-    :return: , tuple:
+    :param A: 2D list of ints:
+        A square cost matrix, where <i|A|j> is the cost to travel from city i to city j where 0 < i, j < len(A).
+        All elements must be >= 0.
+    :param verbose: bool (optional; default is False):
+        Print our extra information - useful for debugging.
+
+    :return:
         np.array: 2-dimensional unitary matrix U.
-        tuple: cycle cost range
+        tuple: Cycle cost range. Will be eeded to convert from phase back to cost.
     """
-    # Normalize all the cycle costs so they are between pi / 2 and 3 * pi / 2 (We don't use the full 0 -> 2 * pi range
-    #  because numerical errors enable wrapping).
-    # Notice we save the min and max so, later on, we can recover the costs from the phases.
-    cycle_costs_min = np.amin(cycle_costs)
-    cycle_costs -= cycle_costs_min + np.pi / 2
-    cycle_costs_max = np.amax(cycle_costs)
-    cycle_costs *= (3 * np.pi / 2) / np.amax(cycle_costs)
+    n = len(A)  # Number of cities in the problem
+    cycle_costs = np.asarray(find_cycle_costs(A=A), dtype=float)
 
-    print("\ncycle costs after normalization:")
-    print(cycle_costs)
+    # Normalize all the cycle costs so they are in the range [0, 2 * pi).
+    # Notice we could use Durr and Hoyer quantum-enhanced optimation here to compute the min and max values of A.
+    cycle_costs_max = n * np.amax(A)  # This is the largest possbily cycle cost.
+    cycle_costs_min = n * np.amin(A)  # This is the smallest possible cycle cost
 
-    U = np.diag(v=cycle_costs, k=0)
-    U = 1j * U
-    U = np.exp(U)
+    normalized_cycle_costs = cycle_costs - cycle_costs_min
+    normalized_cycle_costs *= 2 * np.pi / cycle_costs_max
+
+    diagonal_elements = np.exp(1j * normalized_cycle_costs)
+    U = np.diag(v=diagonal_elements, k=0)
+
+    if verbose:
+        print("\nCycle costs: " + str(cycle_costs))
+
+        print("Max possible cycle cost: " + str(cycle_costs_max))
+        print("Min possible cycle cost: " + str(cycle_costs_min))
+
+        print("\nCycle costs after normalization:")
+        print(normalized_cycle_costs)
+
+        print("\nDiagonal elements:")
+        print(diagonal_elements)
 
     return U, (cycle_costs_min, cycle_costs_max)
 
 
-def costs_by_phase_estimation(U: ArrayLike, cycle_cost_range: Tuple[int, int]):
+def phase_estimation(U: list[list]):
+    """
+    Using phase estimation, estimate the eigenvalues of U.
+    Since U is a diagonal matrix of complex exponentials, it must be unitary. Also, its eigenvectors must be the
+     standard basis vectors. Also also, the eigenvalues are just the diagonal elements, but let's ignore this.
+
+    :param U: 2d list of complex numbers:
+        A diagonal matrix with costs encoded as phases.
+
+    :return: list of floats:
+        thetas: A list of estimated phases. These phases represent the total costs.
+        From these phases, we could obtain the eignevalues as e^(2pi * i * theta).
     """
 
-    :param U:
-    :param cycle_cost_range:
-    :return:
-    """
-    # Obtain the cycle costs from U by with phase estimation.
-    number_of_cycles = len(U)  # All eigenstates of U correspond to Hamiltonian cycles.
+    # The number of target wires required depends on the size of U
+    n_target_wires = math.ceil(math.log(len(U_), 2))
 
-    cycle_costs = np.fill(shape=number_of_cycles, fill_value=0.0)
+    # We get to choose the number of estimation wires to use. The error in our estimate will decrease exponentially
+    #  with the number of estimation qubits.
+    n_estimation_wires = 5
 
-    for i in range(number_of_cycles):
+    thetas = np.full(shape=len(U_), fill_value=np.nan)
+
+    for i in range(len(U_)):
         # Perform phase estimation to solve U|i> = lambda |i>
-        a = 1  # TODO
+
+        target_wires = list(range(0, n_target_wires))
+        estimation_wires = range(n_target_wires, n_target_wires + n_estimation_wires)
+
+        # Create a device, node, and execute the quntum circuit.
+        device = qml.device("default.qubit", wires=n_estimation_wires + 1)
+        qnode = qml.qnode(func=phase_estimation_circuit, device=device)()
+
+        values = qnode.__call__(unitary=U, target_wires=target_wires, estimation_wires=estimation_wires, i=i)
+        # print("values:")
+        # print(values)
+
+        # Find the index of the largest value in the probability distribution and divide that number by 2^n.
+        theta = np.argmax(values) / 2 ** n_estimation_wires
+        # eigenvalue = np.exp(2 * np.pi * 1j * theta)
+
+        thetas[i] = theta
+
+    return thetas
 
 
-    # Now that we have the eigenvalue phases, convert them back into costs.
-    cycle_costs *= cycle_cost_range[1] / (3 * np.pi / 2)
-    cycle_costs += cycle_cost_range[0] + np.pi / 2
+def phase_estimation_circuit(unitary: list[list[float]], target_wires: list[int], estimation_wires: list[int],
+                             i: int) -> float:
+    """
+    Phase estimation circuit.
 
-    return cycle_costs
+    :param unitary: 2D list of floats:
+        The diagonal unitary of which we are trying to estimate the phases.
+    :param target_wires: list of ints:
+        Our work register, this will hold the answer.
+    :param estimation_wires: List of ints:
+        Loaded with an eigenstate corresponding to the phase we are after.
+    :param i: int:
+        We will find the phase corresponding the ith basis vector.
+
+    :return: float:
+        A list of lenght len(U) containing the estimated phase.
+    """
+    # Prepare target wires in the ith basis vector.
+    qml.BasisState(np.asarray([i]), wires=target_wires)
+
+    QuantumPhaseEstimation(unitary, target_wires=target_wires, estimation_wires=estimation_wires)
+
+    return qml.probs(estimation_wires)
 
 
 if __name__ == "__main__":
@@ -74,18 +142,26 @@ if __name__ == "__main__":
     print("n = 3:")
     A3 = np.asarray([[3, 7, 2],
                      [5, 12, 9],
-                     [17, 1, np.inf]])
+                     [17, 1, 6]])
 
     print("\nA:")
     print(A3)
 
-    print("\nCosts:")
-    costs = find_cycle_costs(A=A3)
-    print(costs)
-
-    U_, cycle_cost_range_ = build_U(cycle_costs=costs)
-    print("\nCycle cost range:")
+    # Using A, build a unitary matrix where the costs are encoded as phases.
+    U_, cycle_cost_range_ = build_U(A=A3, verbose=True)
+    print("\nUnitary matrix U:")
     print(U_)
+    print("Cycle cost range: " + str(cycle_cost_range_[0]) + ", " + str(cycle_cost_range_[1]))
+
+    # Use phase estimation to obtain the phases (phases encode total costs).
+    thetas = phase_estimation(U=U_)
+
+    # Now that we have the phases, convert them back into costs.
+    cycle_costs_found = thetas * cycle_cost_range_[1]
+    cycle_costs_found += cycle_cost_range_[0]
+
+    print("\nOutput phases:")
+    print(cycle_costs_found)
 
     # print("\nn = 4:")
     # A4 = np.asarray([[3, 7, 2, 19],
@@ -95,4 +171,3 @@ if __name__ == "__main__":
     #
     # print("\nA:")
     # print(A4)
-
