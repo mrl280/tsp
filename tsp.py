5051df32de1a6f1626fb27712aae65e91557f5cc
Michael Luciuk <mrl280@mail.usask.ca>
2022-12-17 12:10:09 -0500

    Refactoring into callable tsp() function.

--- tsp.py
+++ tsp.py
@@ -11,30 +11,59 @@ import numpy as np
 from grover_enhanced_minimization import grover_enhanced_minimization
 from phase_estimation import build_U, phase_estimation
 
-print("3 City Example (n = 3):")
-A = np.asarray([[3, 7, 2],
-                [5, 12, 9],
-                [17, 1, 6]])
 
-print("\nA:")
-print(A)
+def tsp(A: list[list[int]]) -> int:
+    """
+    A "quantum-enhanced" TSP solver. Uses a variation of the algorithm presented by Srinivasan and collegues in
+     “Efficient quantum algorithm for solving travelling salesman problem: An IBM quantum experience.”
 
-# Using A, build a unitary matrix where the costs are encoded as phases.
-U, cycle_cost_range = build_U(A=A, verbose=False)
+    # TODO: Return the lowest cost circuit, rather than just the cost of the lowest-cost circuit.
 
-# Use phase estimation to obtain the phases (phases encode total costs).
-thetas = phase_estimation(U=U)
+    :param A: 2d list of ints:
+        A square cost matrix, where <i|A|j> is the cost to travel from city i to city j where 0 < i, j < len(A).
+        All elements must be >= 0.
 
-# Now that we have the phases, convert them back into costs.
-cycle_costs_found = thetas * cycle_cost_range[1]
-cycle_costs_found += cycle_cost_range[0]
+    :return: int:
+        The cost of the lowest-cost Hamiltonian cycle.
+    """
+    # Using A, build a unitary matrix where the costs are encoded as phases.
+    U, cycle_cost_range = build_U(A=A, verbose=False)
 
-print("\nTotal costs:")
-print(cycle_costs_found)
+    # Use phase estimation to obtain the phases (phases encode total costs).
+    thetas = phase_estimation(U=U)
 
-# Right now our Grover enhanced minimization only works for interger lists. Our toal costs should be integer anyway.
-cycle_costs_rounded = [int(item) for item in cycle_costs_found]
-lowest_cost = grover_enhanced_minimization(arr=cycle_costs_rounded, verbose=False)
+    # Now that we have the phases, convert them back into costs.
+    cycle_costs_found = thetas * cycle_cost_range[1]
+    cycle_costs_found += cycle_cost_range[0]
 
-print("\nShortest cost:")
-print(lowest_cost)
+    # Right now our Grover enhanced minimization only works for interger lists. Our toal costs should be integer anyway.
+    cycle_costs_rounded = [int(item) for item in cycle_costs_found]
+    lowest_cost = grover_enhanced_minimization(arr=cycle_costs_rounded, verbose=False)
+
+    return lowest_cost
+
+
+if __name__ == "__main__":
+
+    print("### 3 City Example (n = 3) ###")
+    A = [[0, 7, 2],
+         [5, 0, 9],
+         [17, 1, 0]]
+
+    print("\nA:")
+    print(A)
+
+    print("\nCost of shortest cost circuit:")
+    print(tsp(A=A))
+
+    print("\n### 4 city Example (n = 4) ### ")
+    A = [[3, 7, 2, 19],
+         [5, 12, np.nan, 5],
+         [17, 1, np.inf, 2.4],
+         [3, 13, 13, 12]]
+
+    print("\nA:")
+    print(A)
+
+    print("\nCost of shortest cost circuit:")
+    print(tsp(A=A))
